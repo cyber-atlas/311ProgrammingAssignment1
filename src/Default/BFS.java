fa45a1cbe4f5ae48ef6690235051101d1b9746f7
John Young <younjoh1@iastate.edu>
2018-10-10 21:25:37 -0500

    updates on the BFS stuff

--- src/Default/BFS.java
+++ src/Default/BFS.java
@@ -1,4 +1,40 @@
 package Default;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+
+
 
 public class BFS {
+    HashMap<String, Integer> map = new HashMap<>();
+    private Graph t;
+    Fifo Q;
+    ArrayList<String> Discovered;
+    String root = "root";
+    BFS(Graph G){
+        this.t = G;
+        this.Q = new Fifo();
+        Discovered = new ArrayList<String>();
+    }
+
+    public Graph traverse(Graph t){
+        Q.Enque(root);
+        Discovered.add(root);
+        String extract = Q.Deque();
+
+        return null;
+
+    }
+
+
+
+    private class Graph{
+        ArrayList<String> v = new ArrayList<String>();
+        ArrayList<String> e = new ArrayList<String>();
+        Graph(ArrayList<String> vertex, ArrayList<String> edge){
+            this.v = vertex;
+            this.e = edge;
+        }
+    }
 }
--- src/Default/WikiCrawler.java
+++ src/Default/WikiCrawler.java
@@ -80,7 +80,7 @@ public class WikiCrawler {
 
         //while there is another match
         while (match.find()){
-            //returns the subrsting
+            //returns the substring
             links.add(match.group().substring(1,match.group().length()-1));
 
 
